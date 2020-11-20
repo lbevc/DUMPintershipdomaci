a13c272acbace885a7b86c13476a3970ff0c6451
lbevc <bevc.lana@gmail.com>
2020-11-20 23:06:17 +0100

    Added initial project architecture.

---
+++ DUMPdomaci/DUMPdomaci/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DUMPdomaci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
