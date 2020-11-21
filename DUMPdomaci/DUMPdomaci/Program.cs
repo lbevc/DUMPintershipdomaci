0054d1b47e82663ec5a98937c34ab1918b79cfbc
lbevc <bevc.lana@gmail.com>
2020-11-21 22:30:39 +0100

    Added console writeline for third commit.

--- DUMPdomaci/DUMPdomaci/Program.cs
+++ DUMPdomaci/DUMPdomaci/Program.cs
@@ -7,7 +7,7 @@ namespace DUMPdomaci
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("3.commit");
+            Console.WriteLine("3.commit.");
         }
     }
 }
